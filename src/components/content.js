88c042dd00f2ccea02694e60eea56a5217042852
sam <sam.burrell@gmail.com>
2019-05-20 23:14:55 +0100

    added soundcloud

--- src/components/content.js
+++ src/components/content.js
@@ -1,35 +1,22 @@
 import React from "react"
+import Iframe from "react-iframe"
 
-import feature from "../images/feature.png"
 import SectionHeader from "./section-header"
-import { COLORS } from "../styles/constants"
 
 const Content = () => (
-  <div style={{ padding: "4rem 1rem", textAlign: "center" }}>
+  <div style={{ maxWidth: 440, padding: "4rem 1rem", textAlign: "center" }}>
     <SectionHeader
-      title="Minimal Features"
-      description="Don't spend time ripping out unneeded plugins and bloat."
+      title="Desafinado"
+      description="Tuneful jazz, standards & latin. As a duo, trio, 4 piece, or 5 piece."
     />
-    <content
-      style={{
-        display: "grid",
-        alignItems: "center",
-        justifyContent: "center",
-        gridTemplateColumns: "repeat(auto-fit, minmax(240px, 340px))",
-      }}
-    >
-      <div>
-        <h3>What you need to Start</h3>
-        <p style={{ color: COLORS.gray }}>
-          Includes plugins for analytics, building sitemaps, and optimizing
-          images
-        </p>
-      </div>
-      <div>
-        <img src={feature} alt="a blank card floating over colorful graphics" />
-      </div>
-    </content>
+    <Iframe
+      url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/784420671&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      width="100%"
+      height="450"
+      display="initial"
+      position="relative"
+      frameBorder="0"
+    />
   </div>
 )
-
 export default Content
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import React from "react"
 
+import desafinado from "../images/desafinado.jpeg"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Hero from "../components/hero"
 import Content from "../components/content"
 
 const IndexPage = () => (
   <Layout>
-    <SEO />
-    <Hero />
-    <Content />
+    <div style={{ backgroundImage: { desafinado } }}>
+      <SEO />
+      <Content />
+    </div>
   </Layout>
 )
 
