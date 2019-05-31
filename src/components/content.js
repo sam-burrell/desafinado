ebe2f9749a3f17ef4777f97266abed01c5b993f9
Sam Burrell <sam.burrell@signalmedia.co>
2019-05-31 17:05:55 +0100

    grid...

--- src/components/content.js
+++ src/components/content.js
@@ -2,25 +2,32 @@ import React from "react"
 import Iframe from "react-iframe"
 
 import Header from "./header"
-import Footer from "./footer"
-
+import Image from "./image"
 
 const Content = () => (
-  <div style={{ padding: "4rem 1rem", textAlign: "center" }}>
+  <div style={{ padding: "4rem 1rem", textAlign: "center", maxWidth: "60rem" }}>
     <Header
       title="Desafinado"
       description="Tuneful jazz, standards & latin. As a duo, trio, 4 piece, or 5 piece."
     />
-    <Iframe
-      url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/784420671&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
-      width="100%"
-      height="450"
-      display="initial"
-      position="relative"
-      frameBorder="0"
-    />
+    <div style={{display: "grid"}}>
+      <div style ={{gridColumn: 1}}>
+        <Image/>
+      </div>
+      <div style ={{gridColumn: 2}}>
+      <Iframe
+        url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/784420671&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        width="300"
+        height="450"
+        display="initial"
+        position="relative"
+        frameBorder="0"
+      />
+      </div>
+    </div>
     <div>
-      Desafinado is ideal for your club, function, restaurant, bar or private party when you want good music that allows easy conversation
+      Desafinado is ideal for your club, function, restaurant, bar or private
+      party when you want good music that allows easy conversation
     </div>
   </div>
 )
