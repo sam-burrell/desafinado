65e7c9b653115f9a2d054ea71008953261c09697
sam <sam.burrell@gmail.com>
2019-06-14 21:38:51 +0100

    fix

--- src/components/content.js
+++ src/components/content.js
@@ -5,24 +5,24 @@ import Header from "./header"
 import Image from "./image"
 
 const Content = () => (
-  <div style={{ padding: "4rem 1rem", textAlign: "center", maxWidth: "60rem" }}>
+  <div style={{ padding: "2rem 1rem", textAlign: "center"}}>
     <Header
       title="Desafinado"
       description="Tuneful jazz, standards & latin. As a duo, trio, 4 piece, or 5 piece."
     />
-    <div style={{display: "grid"}}>
-      <div style ={{gridColumn: 1}}>
+    <div className="container">
+      <div>
         <Image/>
       </div>
-      <div style ={{gridColumn: 2}}>
-      <Iframe
-        url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/784420671&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
-        width="300"
-        height="450"
-        display="initial"
-        position="relative"
-        frameBorder="0"
-      />
+      <div>
+        <Iframe
+          url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/784420671&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          width="100%"
+          height="450"
+          display="initial"
+          position="relative"
+          frameBorder="0"
+        />
       </div>
     </div>
     <div>
