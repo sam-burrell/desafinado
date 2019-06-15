563e9b6b6e539894dac9b56216a3941fdabd6b50
sam <sam.burrell@gmail.com>
2019-06-15 12:50:59 +0100

    updated

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ const Header = ({ title, description, email, phone }) => (
     <h2>{title}</h2>
     <p style={{ maxWidth: "800px", color: "black" }}>{description}</p>
     <p>
-      <a href="mailto:{email}">{email}</a>
+      email: <a href="mailto:{email}">{email}</a>
     </p>
     <p>
-      <a href="tel:+07747123322">07747 123 322</a>
+      phone: <a href="tel:+07747123322">07747 123 322</a>
     </p>
   </>
 )
